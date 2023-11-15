abe037a40d3bf4f41e4941506304f9baa6507592
yuryskalin <150951259+yuryskalin@users.noreply.github.com>
2023-11-15 20:44:27 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Мама, я тебя люблю ❤️';
+});
+
+console.log(1);
